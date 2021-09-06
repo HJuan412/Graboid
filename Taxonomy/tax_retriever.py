ae7fd50e88f091d55d7919ebac37d47c3916fd40
HJuan412 <hernan.juan@gmail.com>
2021-09-06 19:23:29 -0300

    Updated tax_retriever, now merges acc2taxID tables

--- Taxonomy/tax_retriever.py
+++ Taxonomy/tax_retriever.py
@@ -9,20 +9,23 @@ This script generates the taxonomic information for each entry in the test files
 """
 
 import pandas as pd
+import sys
+sys.path.append('../') # do this to import toolkit
 import toolkit as tools
 
 def make_taxid_tab(seqfile, acc2taxtab1, acc2taxtab2, outfile):
+    # get accessions present in fasta file
     seqdict = tools.make_seqdict(seqfile)
     acc_list = list(seqdict.keys())
-    
+
+    # get acc2axid tables, merge them
     tab1 = pd.read_csv(acc2taxtab1, sep = '\t', header = None)
     tab2 = pd.read_csv(acc2taxtab2, sep = '\t', header = None)
-    
-    taxid_tab = pd.Series(index = acc_list)
-    
-    for tab in [tab1, tab2]:
-        sub_tab = tab.loc[tab[0].isin(acc_list), [0, 2]]
-        for acc, taxID in zip(sub_tab[0].to_list(), sub_tab[2].to_list()):
-            taxid_tab.at[acc] = taxID
-    
+    merged_tab = pd.concat((tab1, tab2))
+
+    # get matches
+    sub_tab = merged_tab.loc[(merged_tab[0].isin(acc_list)) | (merged_tab[1].isin(acc_list)), [1, 2]]
+
+    # generate and store table
+    taxid_tab = sub_tab.loc[:, [1,2]].set_index(1).astype(int)
     taxid_tab.to_csv(outfile)

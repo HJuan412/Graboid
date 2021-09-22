84db034ae3304d2998f35ab61f73c3cbc1eafb7b
HJuan412 <hernan.juan@gmail.com>
2021-09-22 12:22:54 -0300

    Generated individual survey functions for all databases

--- Databases/db_survey.py
+++ Databases/db_survey.py
@@ -4,32 +4,45 @@
 Created on Fri Sep 17 13:05:58 2021
 
 @author: hernan
-Build and maintain databases
+Survey databases and download search summaries
 """
 
 #%% libraries
 from Bio import Entrez
-from Bio import SeqIO
-from datetime import datetime
-import os
 import urllib3
 
-#%% variables
-Entrez.email = "hernan.juan@gmail.com"
-Entrez.api_key = "7c100b6ab050a287af30e37e893dc3d09008"
+# TODO: remove this
+# #%% variables
+# Entrez.email = "hernan.juan@gmail.com"
+# Entrez.api_key = "7c100b6ab050a287af30e37e893dc3d09008"
 
-#%% List current databases
-data = '/home/hernan/PROYECTOS/Maestria/Data/18S_Nematoda.fasta'
-def make_acclist(data):
-    with open(data, 'r') as handle:
-        data_parser = SeqIO.parse(handle, 'fasta')
-        acc_list = [record.id for record in data_parser]
-    return acc_list
+# #%% List current databases
+# data = '/home/hernan/PROYECTOS/Maestria/Data/18S_Nematoda.fasta'
+# def make_acclist(data):
+#     with open(data, 'r') as handle:
+#         data_parser = SeqIO.parse(handle, 'fasta')
+#         acc_list = [record.id for record in data_parser]
+#     return acc_list
 
-acc_list = make_acclist(data)
+# acc_list = make_acclist(data)
 
 #%% functions
 def dl_and_save(apiurl, outfile):
+    """
+    Connects to a database (BOLD or ENA) and downloads search results
+
+    Parameters
+    ----------
+    apiurl : str
+        Generated instructions for the api.
+    outfile : str
+        Output file name.
+
+    Returns
+    -------
+    None.
+
+    """
     # connect to server & send request
     http = urllib3.PoolManager()
     r = http.request('GET', apiurl, preload_content = False)
@@ -42,43 +55,134 @@ def dl_and_save(apiurl, outfile):
     # close connection
     r.release_conn()
     return
+        
+def generate_filename(tax, mark, db):
+    filename = f'{tax}_{mark}_{db}.summ'
+    return filename
 
-def ncbi_search(taxon, marker, outfile):
+# url generators
+def get_bold_url(taxon):
+    apiurl = f'http://www.boldsystems.org/index.php/API_Public/specimen?taxon={taxon}&format=tsv'
+    return apiurl
+
+def get_ena_url(taxon, marker):
+    apiurl = f'https://www.ebi.ac.uk/ena/browser/api/tsv/textsearch?domain=embl&result=sequence&query=%22{taxon}%22%20AND%20%22{marker}%22'
+    return apiurl
+
+# survey
+def survey_bold(taxon, outfile):
+    """
+    Performs a search using the BOLD api
+
+    Parameters
+    ----------
+    taxon : str
+        Taxon to search for.
+    outfile : str
+        Output file name.
+
+    Returns
+    -------
+    None.
+
+    """
+    apiurl = get_bold_url(taxon)
+    dl_and_save(apiurl, outfile)
+
+def survey_ena(taxon, marker, outfile):
+    """
+    Performs a search using the ENA api
+
+    Parameters
+    ----------
+    taxon : str
+        Taxon to search for.
+    marker : str
+        Marker to search for.
+    outfile : str
+        Output file name.
+
+    Returns
+    -------
+    None.
+
+    """
+    apiurl = get_ena_url(taxon, marker)
+    dl_and_save(apiurl, outfile)
+
+def survey_ncbi(taxon, marker, outfile):
+    """
+    Performs a search in the NCBI database using the Entrez api
+
+    Parameters
+    ----------
+    taxon : str
+        Taxon to search for.
+    marker : str
+        Marker to search for.
+    outfile : str
+        Output file name.
+
+    Returns
+    -------
+    None.
+
+    """
     search = Entrez.esearch(db='nucleotide', term = f'{taxon}[Organism] AND {marker}[all fields]', idtype = 'acc', retmax="100000")
     search_record = Entrez.read(search)
     with open(outfile, 'w') as handle:
         handle.write('\n'.join(search_record['IdList']))
-        
-def generate_filename(tax, mark, db, timereg = True):
-    t = datetime.now()
-    filename = f'{tax}_{mark}_{db}_{t.day}-{t.month}-{t.year}_{t.hour}-{t.minute}-{t.second}.summ'
-    return filename
 
-#%%
-summary_dir = '/home/hernan/PROYECTOS/Graboid/Databases/Summaries'
-taxons = ['Nematoda', 'Platyhelminthes']
-markers = ['18S', '28S', 'COI']
+#%% main function
+# TODO: delete commented
+# summary_dir = '/home/hernan/PROYECTOS/Graboid/Databases/Summaries'
+# taxons = ['Nematoda', 'Platyhelminthes']
+# markers = ['18S', '28S', 'COI']
 
-bold = True
-ena = False
-ncbi = False
+# bold = True
+# ena = False
+# ncbi = False
 
-for taxon in taxons:
-    # BOLD search
-    if bold:
-        apiurl =f'http://www.boldsystems.org/index.php/API_Public/specimen?taxon={taxon}&format=tsv'
-        outfile = generate_filename(taxon, '', 'BOLD')
-        dl_and_save(apiurl, f'{summary_dir}/{outfile}')
-        # bold_tab = pd.read_csv('nem_BOLD_summ.tsv', sep = '\t', encoding = 'latin-1') # latin-1 to parse BOLD files
+def survey(summ_dir, taxons, markers, bold = True, ena = True, ncbi = True):
+    """
+    
 
-    for marker in markers:
-        # ENA search
-        if ena:
-            apiurl = f'https://www.ebi.ac.uk/ena/browser/api/tsv/textsearch?domain=embl&result=sequence&query=%22{taxon}%22%20AND%20%22{marker}%22'
-            outfile = generate_filename(taxon, marker, 'ENA')
-            dl_and_save(apiurl, )
+    Parameters
+    ----------
+    summ_dir : TYPE
+        DESCRIPTION.
+    taxons : TYPE
+        DESCRIPTION.
+    markers : TYPE
+        DESCRIPTION.
+    bold : TYPE, optional
+        DESCRIPTION. The default is True.
+    ena : TYPE, optional
+        DESCRIPTION. The default is True.
+    ncbi : TYPE, optional
+        DESCRIPTION. The default is True.
 
-        # NCBI search
-        if ncbi:
-            outfile = generate_filename(taxon, marker, 'NCBI')
-            ncbi_search(taxon, marker, f'{summary_dir}/{outfile}')
+    Returns
+    -------
+    None.
+
+    """
+    for taxon in taxons:
+        # BOLD search
+        if bold:
+            apiurl = f'http://www.boldsystems.org/index.php/API_Public/specimen?taxon={taxon}&format=tsv'
+            outfile = generate_filename(taxon, '', 'BOLD')
+            dl_and_save(apiurl, f'{summ_dir}/{outfile}')
+            # bold_tab = pd.read_csv('nem_BOLD_summ.tsv', sep = '\t', encoding = 'latin-1') # latin-1 to parse BOLD files
+    
+        for marker in markers:
+            # ENA search
+            if ena:
+                apiurl = f'https://www.ebi.ac.uk/ena/browser/api/tsv/textsearch?domain=embl&result=sequence&query=%22{taxon}%22%20AND%20%22{marker}%22'
+                outfile = generate_filename(taxon, marker, 'ENA')
+                dl_and_save(apiurl, f'{summ_dir}/{outfile}')
+    
+            # NCBI search
+            if ncbi:
+                outfile = generate_filename(taxon, marker, 'NCBI')
+                survey_ncbi(taxon, marker, f'{summ_dir}/{outfile}')

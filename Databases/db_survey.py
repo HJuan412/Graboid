8bb1ab61699ba2b7261c281470f427dc80198a9c
HJuan412 <hernan.juan@gmail.com>
2021-10-12 10:43:32 -0300

    Modifyied surveyors, added active parameter, this avoids needless repeating of BOLD surveys

--- Databases/db_survey.py
+++ Databases/db_survey.py
@@ -11,115 +11,8 @@ Survey databases and download search summaries
 from Bio import Entrez
 import urllib3
 
-#%% functions
-# def dl_and_save(apiurl, outfile):
-#     """
-#     Connects to a database (BOLD or ENA) and downloads search results
-
-#     Parameters
-#     ----------
-#     apiurl : str
-#         Generated instructions for the api.
-#     outfile : str
-#         Output file name.
-
-#     Returns
-#     -------
-#     None.
-
-#     """
-#     # connect to server & send request
-#     http = urllib3.PoolManager()
-#     r = http.request('GET', apiurl, preload_content = False)
-    
-#     # stream request and store in outfile
-#     # TODO: handle interruptions
-#     with open(outfile, 'ab') as handle:
-#         for chunk in r.stream():
-#             handle.write(chunk)
-#     # close connection
-#     r.release_conn()
-#     return
-        
-# def generate_filename(tax, mark, db):
-#     filename = f'{tax}_{mark}_{db}.summ'
-#     return filename
-
-# # url generators
-# def get_bold_url(taxon):
-#     apiurl = f'http://www.boldsystems.org/index.php/API_Public/specimen?taxon={taxon}&format=tsv'
-#     return apiurl
-
-# def get_ena_url(taxon, marker):
-#     apiurl = f'https://www.ebi.ac.uk/ena/browser/api/tsv/textsearch?domain=embl&result=sequence&query=%22{taxon}%22%20AND%20%22{marker}%22'
-#     return apiurl
-
-# # survey
-# def survey_bold(taxon, outfile):
-#     """
-#     Performs a search using the BOLD api
-
-#     Parameters
-#     ----------
-#     taxon : str
-#         Taxon to search for.
-#     outfile : str
-#         Output file name.
-
-#     Returns
-#     -------
-#     None.
-
-#     """
-#     apiurl = get_bold_url(taxon)
-#     dl_and_save(apiurl, outfile)
-
-# def survey_ena(taxon, marker, outfile):
-#     """
-#     Performs a search using the ENA api
-
-#     Parameters
-#     ----------
-#     taxon : str
-#         Taxon to search for.
-#     marker : str
-#         Marker to search for.
-#     outfile : str
-#         Output file name.
-
-#     Returns
-#     -------
-#     None.
-
-#     """
-#     apiurl = get_ena_url(taxon, marker)
-#     dl_and_save(apiurl, outfile)
-
-# def survey_ncbi(taxon, marker, outfile):
-#     """
-#     Performs a search in the NCBI database using the Entrez api
-
-#     Parameters
-#     ----------
-#     taxon : str
-#         Taxon to search for.
-#     marker : str
-#         Marker to search for.
-#     outfile : str
-#         Output file name.
-
-#     Returns
-#     -------
-#     None.
-
-#     """
-#     search = Entrez.esearch(db='nucleotide', term = f'{taxon}[Organism] AND {marker}[all fields]', idtype = 'acc', retmax="100000")
-#     search_record = Entrez.read(search)
-#     with open(outfile, 'w') as handle:
-#         handle.write('\n'.join(search_record['IdList']))
-
 #%% main function
-def survey(summ_dir, taxons, markers, bold = True, ena = True, ncbi = True):
+def survey(summ_dir, taxons, markers, bold = True, ena = True, ncbi = True, verbose = True):
     """
     
 
@@ -143,43 +36,30 @@ def survey(summ_dir, taxons, markers, bold = True, ena = True, ncbi = True):
     None.
 
     """
-    surv_list = []
-    if bold:
-        surv_list.append(BOLD_surveyor(summ_dir))
-    if ena:
-        surv_list.append(ENA_surveyor(summ_dir))
-    if ncbi:
-        surv_list.append(NCBI_surveyor(summ_dir))
+    
+    bold_survey = BOLD_surveyor(summ_dir, bold)
+    ena_survey = ENA_surveyor(summ_dir, ena)
+    ncbi_survey = NCBI_surveyor(summ_dir, ncbi)
 
     for taxon in taxons:
+        bold_survey.survey(taxon)
         for marker in markers:
-            for surv in surv_list:
-                surv.survey(taxon, marker)
+            if verbose:
+                print(f'Surveying for {taxon}, {marker}')
+            ena_survey.survey(taxon, marker)
+            ncbi_survey.survey(taxon, marker)
 
 #%% classes
 class Surveyor():
-    def __init__(self, out_dir):
+    def __init__(self, out_dir, active = False):
         self.out_dir = out_dir
+        self.active = active
     
-    def dl_and_save(self, apiurl, out_file):
-        # connect to server & send request
-        http = urllib3.PoolManager()
-        r = http.request('GET', apiurl, preload_content = False)
-        
-        # stream request and store in outfile
-        # TODO: handle interruptions
-        with open(out_file, 'ab') as handle:
-            for chunk in r.stream():
-                handle.write(chunk)
-        # close connection
-        r.release_conn()
-        return
+    def activate(self):
+        self.active = True
+    def deactivate(self):
+        self.active = False
 
-
-    def survey(self, taxon, marker = None):
-        out_file = self.generate_filename(taxon. marker)
-        apiurl = self.get_url(taxon, marker)
-        self.dl_and_save(apiurl, out_file)
     def dl_and_save(self, apiurl, out_file):
         """
         Connects to a database (BOLD or ENA) and downloads search results
@@ -210,9 +90,11 @@ class Surveyor():
         return
 
     def survey(self, taxon, marker = None):
-        out_file = self.generate_filename(taxon, marker)
-        apiurl = self.get_url(taxon, marker)
-        self.dl_and_save(apiurl, out_file)
+        if self.active:
+            out_file = self.generate_filename(taxon, marker)
+            apiurl = self.get_url(taxon, marker)
+            self.dl_and_save(apiurl, out_file)
+            return
         return
 
 class BOLD_surveyor(Surveyor):
@@ -235,13 +117,15 @@ class ENA_surveyor(Surveyor):
 
 class NCBI_surveyor(Surveyor):
     def generate_filename(self, taxon, marker):
-        filename = f'{taxon}_{marker}_NCBI.summ'
+        filename = f'{self.out_dir}/{taxon}_{marker}_NCBI.summ'
         return filename
     
     def survey(self, taxon, marker):
-        out_file = self.generate_filename(taxon, marker)
-        search = Entrez.esearch(db='nucleotide', term = f'{taxon}[Organism] AND {marker}[all fields]', idtype = 'acc', retmax="100000")
-        search_record = Entrez.read(search)
-        with open(out_file, 'w') as handle:
-            handle.write('\n'.join(search_record['IdList']))
+        if self.active:
+            out_file = self.generate_filename(taxon, marker)
+            search = Entrez.esearch(db='nucleotide', term = f'{taxon}[Organism] AND {marker}[all fields]', idtype = 'acc', retmax="100000")
+            search_record = Entrez.read(search)
+            with open(out_file, 'w') as handle:
+                handle.write('\n'.join(search_record['IdList']))
+            return
         return

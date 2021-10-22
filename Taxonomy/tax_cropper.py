fdd1e27362770cad218bb13f958c96f5b4eeaf67
HJuan412 <hernan.juan@gmail.com>
2021-10-22 15:54:32 -0300

    Created tax_cropper.py, used to crop acc2taxid tables to only include present sequences

---
+++ Taxonomy/tax_cropper.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 14:38:24 2021
+
+@author: hernan
+Perform reciprocal grep between accession2taxid files and Acc_lists
+"""
+
+#%% libraries
+from tax_NCBI_fetcher import check_outdir
+import os
+import pandas as pd
+import subprocess
+
+#%% variables
+acc_dir = '/home/hernan/PROYECTOS/Graboid/Databases/13_10_2021-20_15_58/Acc_lists'
+gb_file = '/home/hernan/PROYECTOS/Graboid/Taxonomy/Test2/nucl_gb.accession2taxid'
+wgs_file = '/home/hernan/PROYECTOS/Graboid/Taxonomy/Test2/nucl_wgs.accession2taxid'
+
+#%% functions
+def get_acclist(file, tmp_dir):
+    tab = pd.read_csv(file)
+    acclist = tab.iloc[:,0].tolist()
+    out_path = f'{tmp_dir}/{file.split("/")[-1]}.tmp'
+    with open(out_path, 'w') as handle:
+        handle.write('\n'.join(acclist))
+
+def grep_acc2taxid(acc_list, file, out_file):
+    grep_cline = ['grep', '-w', '-f', acc_list, file]
+    with open(out_file, 'a') as out_handle:
+        process = subprocess.Popen(grep_cline, stdout = out_handle)
+        process.wait()
+        print('Done!')
+#%% classes
+class Cropper():
+    def __init__(self, out_dir, acc_file, tax, mark):
+        self.out_dir = out_dir
+        check_outdir(out_dir)
+        self.make_acclist(acc_file)
+        self.check_acc2taxid_files()
+        self.out_file = f'{out_dir}/{tax}_{mark}_acc2taxid.tsv'
+
+    def make_acclist(self, acc_file):
+        tab = pd.read_csv(acc_file)
+        acclist = tab.iloc[:,0].tolist()
+        self.acc_list = f'{self.out_dir}/{acc_file.split("/")[-1]}.tmp'
+        with open(self.acc_list, 'w') as out_handle:
+            out_handle.write('\n'.join(acclist))
+        return
+    
+    def clear_acclist(self):
+        if os.path.isfile(self.acc_list):
+            os.remove(self.acc_list)
+    
+    def clear_outfile(self):
+        if os.path.isfile(self.out_file):
+            os.remove(self.out_file)
+
+    def check_acc2taxid_files(self):
+        gb = f'{self.out_dir}/nucl_gb.accession2taxid'
+        wgs = f'{self.out_dir}/nucl_wgs.accession2taxid'
+        if os.path.isfile(gb):
+            self.gb = gb
+        else:
+            self.gb = None
+        if os.path.isfile(wgs):
+            self.wgs = wgs
+        else:
+            self.wgs = None
+    
+    def crop(self):
+        self.clear_outfile()
+        if not self.gb is None:
+            grep_acc2taxid(self.acc_list, self.gb, self.out_file)
+        if not self.wgs is None:
+            grep_acc2taxid(self.acc_list, self.wgs, self.out_file)
+            

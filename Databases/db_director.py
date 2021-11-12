722e1f03e7e089dc25fca285d51beb97668d004f
HJuan412 <hernan.juan@gmail.com>
2021-11-12 14:48:32 -0300

    Created Director class to replace main function (test pending)

--- Databases/db_director.py
+++ Databases/db_director.py
@@ -24,12 +24,12 @@ def generate_dirname():
     dirname = f'{t.day}_{t.month}_{t.year}-{t.hour}_{t.minute}_{t.second}'
     return dirname
 
-def generate_subdirs(dirname):
+def generate_subdirnames(dirname):
     return f'{dirname}/Summaries', f'{dirname}/Sequence_files', f'{dirname}/Taxonomy_files', f'{dirname}/Acc_lists', f'{dirname}/Warnings'
 
 def new_directories():
     dirname = generate_dirname()
-    summ_dir, seq_dir, tax_dir, acc_dir, warn_dir = generate_subdirs(dirname)
+    summ_dir, seq_dir, tax_dir, acc_dir, warn_dir = generate_subdirnames(dirname)
     
     os.mkdir(dirname)
     for sdir in [summ_dir, seq_dir, tax_dir, acc_dir, warn_dir]:
@@ -58,13 +58,76 @@ ncbi = False
 old_accs = None # for lister
 chunk_size = 500 # for fetcher
 #%% Main
+class Director():
+    def __init__(self, db_dir = None, bold = True, ena = False, ncbi = True, email = 'hernan.juan@gmail.com', apikey = '7c100b6ab050a287af30e37e893dc3d09008'):
+        self.db_dir = db_dir
+        self.set_directories()
+        self.create_dirs()
+        self.bold = bold
+        self.ena = ena
+        self.ncbi = ncbi
+        self.t1, self.t2 = get_surv_tools(bold, ena, ncbi)
+        self.set_workers()
+        ftch.set_entrez(email, apikey)
+
+    def set_directories(self):
+        if self.db_dir is None:
+            self.db_dir = generate_dirname()
+        self.summ_dir, self.seq_dir, self.tax_dir, self.acc_dir, self.warn_dir = generate_subdirnames(self.db_dir)
+    
+    def create_dirs(self):
+        for d in [self.db_dir, self.summ_dir, self.seq_dir, self.tax_dir, self.acc_dir, self.warn_dir]:
+            if not os.path.isdir(d):
+                os.mkdir(d)
+    
+    def set_workers(self, taxons, markers):
+        self.surveyor = surv.Surveyor(taxons, markers, self.t1, self.t2, self.summ_dir, self.warn_dir)
+        self.lister = lstr.Lister(self.summ_dir, self.acc_dir, self.warn_dir, old_accs)
+        self.taxer = txdr.TaxDirector(self.tax_dir, self.summ_dir, self.acc_dir)
+        self.fetcher = ftch.Fetcher(self.acc_dir, self.seq_dir, self.warn_dir)
+        self.bold_postprocessor = bdpp.BOLDPostProcessor(self.seq_dir, self.seq_dir, self.warn_dir)
+        self.merger = mrgr.Merger(self.seq_dir, self.seq_dir, self.warn_dir, db_order = ['NCBI', 'BOLD', 'ENA'])
+    
+    def direct_survey(self):
+        self.surveyor.survey()
+    
+    def direct_listing(self):
+        self.lister.build_lists()
+    
+    def direct_tax_reconstruction(self):
+        self.taxer.tax_reconstruct()
+    
+    def direct_fetching(self, chunk_size):
+        self.fetcher.fetch(chunk_size)
+    
+    def direct_bold_pp(self, markers):
+        self.bold_postprocessor.process(markers)
+    
+    def direct_merging(self):
+        self.merger.merge()
+    
+    def direct(self, chunk_size, markers):
+        print('Surveying databases...')
+        self.direct_survey()
+        print('Building accession lists...')
+        self.direct_listing()
+        print('Reconstructing taxonomies...')
+        self.direct_tax_reconstruction()
+        print('Fetching sequences...')
+        self.direct_fetching(chunk_size)
+        print('Processing BOLD files...')
+        self.direct_bold_pp()
+        print('Comparing and merging...')
+        self.direct_merging()
+
+#TODO: once Director class is tested, remove this shit funcition
 def make_database(taxons, markers, bold, ena, ncbi, dirname = None):
     # generate directories
     if dirname is None:
         summ_dir, seq_dir, tax_dir, acc_dir, warn_dir = new_directories()
     else:
         # TODO: en este caso, el directorio ya existe, qué hacer con archivos preexistentes?
-        summ_dir, seq_dir, tax_dir, acc_dir, warn_dir = generate_subdirs(dirname)
+        summ_dir, seq_dir, tax_dir, acc_dir, warn_dir = generate_subdirnames(dirname)
     
     # TODO: handle old directory
     # set email and api key

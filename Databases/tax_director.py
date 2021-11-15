2514572054ecb84a5426a7add15738d4fa49891a
HJuan412 <hernan.juan@gmail.com>
2021-11-15 16:10:21 -0300

    created direct_fetching(), direct_cropping(), direct_reconstruction(), direct_BOLD_reconstruction() and direct() methods to TaxDirector

--- Databases/tax_director.py
+++ Databases/tax_director.py
@@ -10,7 +10,7 @@ Direct taxonomy reconstruction
 from glob import glob
 from tax_NCBI_fetcher import TaxFetcher
 from tax_cropper import Cropper
-from tax_reconstructor import reconstruct_all
+from tax_reconstructor import ReconstructionDirector
 from tax_BOLD_reconstructor import Reconstructor
 
 import pandas as pd
@@ -22,7 +22,47 @@ class TaxDirector():
         self.out_dir = out_dir
         self.summ_dir = summ_dir
         self.acc_dir = acc_dir
+        self.set_workers()
     
+    def set_workers(self):
+        self.fetcher = TaxFetcher(self.out_dir)
+        self.cropper = Cropper(self.out_dir, self.acc_dir)
+        self.rec_director = ReconstructionDirector(self.out_dir, ranks = ['phylum', 'class', 'order', 'family', 'genus', 'species'])
+    
+    def direct_fetching(self):
+        self.fetcher.fetch()
+    
+    def direct_cropping(self):
+        self.cropper.merge_accs(self.acc_dir, self.out_dir)
+        self.cropper.crop()
+        self.cropper.split()
+    
+    def direct_reconstruction(self):
+        self.rec_director.locate_acc2tax()
+        self.rec_director.reconstruct()
+    
+    def direct_BOLD_reconstruction(self):
+        # reconstruct for each file
+        self.bold_reconstructors = [Reconstructor(self.out_dir, file) for file in glob(f'{self.summ_dir}/*BOLD*')]
+        # merge names nodes
+        merged_names = pd.concat([rec.name_tab for rec in self.bold_reconstructors])
+        merged_nodes = pd.concat([rec.node_tab for rec in self.bold_reconstructors])
+        # save names, nodes and taxonomy_dfs tables
+        merged_names.to_csv(f'{self.out_dir}/BOLD_names.tsv', sep = '\t')
+        merged_nodes.to_csv(f'{self.out_dir}/BOLD_nodes.tsv', sep = '\t')
+        for rec in self.bold_reconstructors:
+            rec.taxonomy_df.to_csv(f'{rec.out_prefix}_BOLD_acc2taxid.tsv', sep = '\t')
+    
+    def direct(self):
+        print('Fetching taxonomy files...')
+        self.direct_fetching()
+        print('Cropping taxonomy files...')
+        self.direct_cropping()
+        print('Reconstructing taxonomies...')
+        self.direct_reconstruction()
+        print('Reconstructing BOLD taxonomies...')
+        self.direct_BOLD_reconstruction()
+
     def tax_reconstruct(self):
         # NCBI
         # fetch
@@ -33,7 +73,7 @@ class TaxDirector():
         cropper.crop()
         cropper.split()
         # reconstruct
-        reconstruct_all(self.out_dir)
+        # reconstruct_all(self.out_dir)
         # BOLD
         # reconstruct for each file
         reconstructors = [Reconstructor(file) for file in glob(f'{self.summ_dir}/*BOLD*')]

231dbb7b952c7ba61824e0aa6f4a04c7462d9448
HJuan412 <hernan.juan@gmail.com>
2022-01-12 10:56:53 -0300

    modified window collapser for easier use

--- window_collapser.py
+++ window_collapser.py
@@ -6,21 +6,10 @@ This scripc collapses redundant sequences in a file
 """
 
 #%% libraries
+import matrix_toolkit as mtk
 import numpy as np
 import pandas as pd
-#%% variables
 #%% functions
-def crop_invariant_bases(window):
-    # removes bases with invariant values
-    to_remove = []
-    for idx in range(window.shape[1]):
-        uniq_bases = np.unique(window[:,idx])
-        if len(uniq_bases) == 1:
-            to_remove.append(idx)
-    cropped = np.delete(window, to_remove, 1)
-    
-    return cropped, to_remove
-
 def get_val_idx(array):
     # returns a dictionary of the form {value:[idx of rows with value]} for each unique value in the array
     array_vals = set(array)
@@ -53,36 +42,51 @@ def get_uniq_seqs(matrix, row_idx = None, col_idx = 0):
     return uniq_seqs
 
 #%% classes
-import matrix_toolkit as mtk
-
 class WindowCollapser():
     def __init__(self, tax_file, rank = 'family'):
-        self.mat_file = ''
         self.tax_file = tax_file
         self.rank = rank
         self.mloader = mtk.MatrixLoader()
-        self.tax_tab = pd.DataFrame()
-    
-    def load_matrix(self, mat_file):
+        
+        self.set_matrix()
+        self.collapsed = pd.DataFrame()
+        self.mixed_taxes = {}
+    def set_matrix(self, mat_file = None):
+        # load the data matrix to be used
+        self.mat_file = mat_file
+        self.mloader.clear()
         self.mloader.load(mat_file)
         # MatrixLoader object contains matrix, accession list, n, start and end
-        self.load_tax()
+        self.__set_tax()
     
-    def load_tax(self):
-        tax_tab = pd.read_csv(self.tax_file, index_col = 0, sep = '\t')
-        
-        # these three lines make sure we don't ask for missing records in tax_tab
-        acc_set = set(self.mloader.accs)
-        index_set = set(tax_tab.index)
-        intersect = acc_set.intersection(index_set)
-        
-        self.diff = acc_set.difference(index_set) # stores entries present in the window accessions but not in the tax_table
-        self.tax_tab = tax_tab.loc[intersect]
-    
-    def collapse_windows(self):
+    def collapse(self):
+        if not self.mat_file is None:
+            self.__collapse_windows()
+            self.__get_repeated_seqs()
+            self.__get_unique_seqs()
+            self.__get_collapsed()
+
+    # these methods are private because they should only be used by other methods of the class
+    def __set_tax(self):
+        if self.mloader.matrix is None:
+            self.diff = None
+            self.tax_tab = None
+        else:
+            tax_tab = pd.read_csv(self.tax_file, index_col = 0, sep = '\t')
+            
+            # these three lines make sure we don't ask for missing records in tax_tab
+            acc_set = set(self.mloader.accs)
+            index_set = set(tax_tab.index)
+            intersect = acc_set.intersection(index_set)
+            
+            self.diff = acc_set.difference(index_set) # stores entries present in the window accessions but not in the tax_table
+            self.tax_tab = tax_tab.loc[intersect]
+
+    def __collapse_windows(self):
+        # create dictionary of the form {(sequence):(instance_idxs)}
         self.uniq_dict = get_uniq_seqs(self.mloader.matrix)
     
-    def get_repeated_seqs(self):
+    def __get_repeated_seqs(self):
         # checks if the instances sharing the same sequence are of the same taxon
         # for sequences with mixed taxons, generate new pseudo taxons (stored in the mixed_taxes dictionary)
         
@@ -115,7 +119,7 @@ class WindowCollapser():
         self.pure = pure
         self.mixed_taxes = {v:k for k,v in mixed_taxes.items()}
     
-    def get_unique_seqs(self):
+    def __get_unique_seqs(self):
         unique_seqs = {k:v for k,v in self.uniq_dict.items() if len(v) == 1}
         unique = {}
         for k,v in unique_seqs.items():
@@ -125,35 +129,10 @@ class WindowCollapser():
         
         self.unique = unique
     
-    def get_collapsed(self):
+    def __get_collapsed(self):
         collapsed_mixed = {(k, v[0], v[1]) for k,v in self.mixed.items()}
         collapsed_pure = {(k, v[0], v[1]) for k,v in self.pure.items()}
         collapsed_unique = {(k, v[0], v[1]) for k,v in self.unique.items()}
         
         union = collapsed_mixed.union(collapsed_pure).union(collapsed_unique)
         self.collapsed = pd.DataFrame(union, columns = ['seq', 'tax', 'id'])
-
-class CollapsedTaxon():
-    def __init__(self, sub_window):
-        self.sub_window = sub_window
-        self.cropped, self.constant_bases = crop_invariant_bases(sub_window)
-        self.uniq_seqs = []
-        self.check_single_seq()
-
-    def check_single_seq(self):
-        # checks if there is a single seq present in the given taxon
-        self.single = False
-        if len(self.cropped) == 0:
-            self.uniq_seqs.append(self.sub_window[0])
-            self.single = True
-        
-#%% test
-tax_file = 'Databases/12_11_2021-23_15_53/Taxonomy_files/Nematoda_18S_tax.tsv'
-test_file = 'Dataset/12_11_2021-23_15_53/Matrices/Nematoda/18S/17_aln_256-355_n12260.mat'
-wc = WindowCollapser(tax_file)
-wc.load_matrix(test_file)
-wc.collapse_windows()
-wc.get_repeated_seqs()
-wc.get_unique_seqs()
-wc.get_collapsed()
-# tab = pd.DataFrame(columns = ['Seq', 'Tax', 'instances'])

92dd30434923103fa60a02e1802b4782733f7ba9
HJuan412 <hernan.juan@gmail.com>
2022-01-12 15:36:33 -0300

    Created abstract classes for distance calculation and voting, facilitats introducing new modes

--- gb_classify.py
+++ gb_classify.py
@@ -7,10 +7,107 @@ Created on Tue Nov 23 13:44:10 2021
 """
 
 #%% libraries
+from abc import ABC, abstractmethod
 from gb_cost_matrix import cost_matrix
 import gb_preprocess as pp
 import numpy as np
 import pandas as pd
+
+#%% dist calculators
+class DistCalculator(ABC):
+    def __init__(self, data, query, cost_matrix = None):
+        self.data = data
+        self.query = query
+        self.cost_matrix = cost_matrix
+        self.dists = np.zeros((self.query.shape[0], self.data.shape[0]))
+
+    @abstractmethod
+    def get_dist(self):
+        pass
+
+class DistByID(DistCalculator):
+    def get_dist(self):
+        
+        for idx, q in enumerate(self.query):
+            q_dist = (1-(q == self.data)).sum(axis = 1)
+            self.dists[idx,:] = q_dist
+
+class DistByCost(DistCalculator):
+    def get_dist(self):
+        for idx0, query in enumerate(self.query):
+            for idx1, ref in enumerate(self.data):
+                q_cost = 0
+                for q, s in zip(query, ref):
+                    q_cost += self.cost_matrix[q, s]
+                self.dists[idx0, idx1] = q_cost
+
+#%% voters
+class Voter(ABC):
+    def __init__(self, K, distances, data_classes):
+        self.K = K
+        self.distances = distances
+        self.data_classes = data_classes
+        self.classif = False
+
+    @abstractmethod
+    def classify(self):
+        pass
+
+class MajorityVote(Voter):
+    def classify(self):
+        self.classif = pd.DataFrame(columns = ['Code', 'NN'])
+        for idx, q_dist in enumerate(self.distances):
+            sorted_dists = np.argsort(q_dist)
+            k_idx = sorted_dists[:self.K]
+            k_taxes = self.data_classes[k_idx]
+    
+            taxes, counts = np.unique(k_taxes, return_counts = True)
+            most_abundant = np.argsort(counts)[-1]
+            self.classif.at[idx,:] = [taxes[most_abundant], counts[most_abundant]]
+
+class WeightedVote(Voter):
+    def classify(self):
+        self.classif = pd.DataFrame(columns = ['Query', 'Code', 'Weight'])
+        for q_idx, q_dist in enumerate(self.distances):
+            sorted_dists = np.argsort(q_dist)
+            k_idx = sorted_dists[:self.K]
+            k_taxes = self.data_classes[k_idx]
+            w_dict = {tax:0 for tax in k_taxes}
+            
+            for idx in k_idx:
+                tax = self.data_classes[idx]
+                weight = q_dist[idx]
+                w_dict[tax] += weight
+            
+            for k,v in w_dict.items():
+                self.classif = self.classif.append(pd.Series({'Query':q_idx, 'Code':k, 'Weight':v}), ignore_index = True)
+
+#%% testing
+import window_collapser
+
+test_file = 'Dataset/12_11_2021-23_15_53/Matrices/Nematoda/18S/17_aln_256-355_n12260.mat'
+tax_file = 'Databases/12_11_2021-23_15_53/Taxonomy_files/Nematoda_18S_tax.tsv'
+
+wc = window_collapser.WindowCollapser(tax_file)
+wc.set_matrix(test_file)
+wc.collapse()
+matrix = np.array(wc.collapsed['seq'].tolist())
+
+# distance calculators
+dbId = DistByID(matrix[2:], matrix[:2].reshape((-1, 100)))
+dbId.get_dist()
+
+cost_mat = cost_matrix(1, 2)
+dbcost = DistByCost(matrix[2:], matrix[:2].reshape((-1, 100)), cost_mat)
+dbcost.get_dist()
+# voters
+majVote = MajorityVote(10, dbId.dists, wc.collapsed['tax'].to_numpy())
+majVote = MajorityVote(10, dbcost.dists, wc.collapsed['tax'].to_numpy())
+majVote.classify()
+
+weiVo = WeightedVote(10, dbId.dists, wc.collapsed['tax'].to_numpy())
+weiVo = WeightedVote(10, dbcost.dists, wc.collapsed['tax'].to_numpy())
+weiVo.classify()
 
 #%% classes
 class Classifier():

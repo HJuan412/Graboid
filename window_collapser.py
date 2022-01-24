6522b19df0859f6bd424ace21bbf4c0aaab99e50
HJuan412 <hernan.juan@gmail.com>
2022-01-24 15:50:31 -0300

    Modified collapse pipeline, matrix, report and taxonomy are separate attributes. TODO: remove test cell

--- window_collapser.py
+++ window_collapser.py
@@ -40,18 +40,13 @@ def get_uniq_seqs(matrix, row_idx = None, col_idx = 0):
         uniq_seqs.update(get_uniq_seqs(matrix, row_idx1, col_idx + 1))
 
     return uniq_seqs
-
 #%% classes
 class WindowCollapser():
-    def __init__(self, tax_file, rank = 'family'):
+    def __init__(self, tax_file):
         # tax_file should be a file located in the Taxonomy_files dir in a downloaded database, name should be <taxon>_<marker>_tax.tsv
         self.tax_file = tax_file
-        self.rank = rank
         self.mloader = mtk.MatrixLoader()
-        
         self.set_matrix()
-        self.collapsed = pd.DataFrame() # contains the report of collapsed sequences
-        self.mixed_taxes = {} # contains the members of mixed_taxes
 
     def set_matrix(self, mat_file = None):
         # load the data matrix to be used, if mat_file is None clears existing matrix
@@ -60,23 +55,24 @@ class WindowCollapser():
         self.mloader.load(mat_file)
         # MatrixLoader object contains matrix, accession list, n, start and end
         self.__set_tax()
-        self.collapsed = pd.DataFrame()
-        self.mixed_taxes = {}
     
     def collapse(self):
-        # collapse the loaded matrix and generate the collapsed and mixed_taxes reports
         if not self.mat_file is None:
             self.__collapse_windows()
-            self.__get_repeated_seqs()
-            self.__get_unique_seqs()
-            self.__get_collapsed()
+            self.__get_unique_repeated()
+            self.__unpack()
+            self.__uniq_taxes()
+            self.__mixd_taxes()
+            self.__build_tax_tab()
 
     # these methods are private because they should only be used by other methods of the class
     def __set_tax(self):
         if self.mloader.matrix is None:
+            # no matrix loaded, clear records
             self.diff = None
             self.tax_tab = None
         else:
+            # load taxonomy table and extract relevant entries
             tax_tab = pd.read_csv(self.tax_file, index_col = 0, sep = '\t')
             
             # these three lines make sure we don't ask for missing records in tax_tab
@@ -91,53 +87,65 @@ class WindowCollapser():
         # create dictionary of the form {(sequence):(instance_idxs)}
         self.uniq_dict = get_uniq_seqs(self.mloader.matrix)
     
-    def __get_repeated_seqs(self):
-        # checks if the instances sharing the same sequence are of the same taxon
-        # for sequences with mixed taxons, generate new pseudo taxons (stored in the mixed_taxes dictionary)
-        
-        repeated = {k:v for k,v in self.uniq_dict.items() if len(v) != 1} # contains all sequences with more than one instance in the windows
-        mixed = {}
-        pure = {}
-        
-        mixed_taxes = {}
-        n_mixed = 0
-        for k,v in repeated.items():
-            accs = {self.mloader.accs[i] for i in v}
-            taxons = tuple(self.tax_tab.loc[accs, self.rank].unique())
-            
-            if len(taxons) > 1:
-                # mutliple taxons share this sequence
-                # check if a pseduo taxon with the same taxon combination already exists (if not create a new one)
-                if taxons in mixed_taxes.keys():
-                    tax_id = mixed_taxes[taxons] # select the existing pseudo-tax id for the current sequence
-                else:
-                    # create a new pseudo taxon for the current taxon mix
-                    tax_id = f'mix_{n_mixed}'
-                    mixed_taxes[taxons] = tax_id
-                    n_mixed += 1
-                mixed[k] = tax_id, v
-            else:
-                # all instances sharing the sequence are of the same taxon (it is pure)
-                pure[k] = taxons[0], v
-        
-        self.mixed = mixed
-        self.pure = pure
-        self.mixed_taxes = {v:k for k,v in mixed_taxes.items()}
-    
-    def __get_unique_seqs(self):
-        unique_seqs = {k:v for k,v in self.uniq_dict.items() if len(v) == 1}
+    def __get_unique_repeated(self):
+        # sorts repeated and unique sequences
+        repeated = {}
         unique = {}
-        for k,v in unique_seqs.items():
-            acc = self.mloader.accs[v[0]]
-            tax = self.tax_tab.loc[acc, self.rank]
-            unique[k] = tax, v
-        
+        for k,v in self.uniq_dict.items():
+            if len(v) != 1:
+                repeated[k] = v # contains all sequences with more than one instance
+            else:
+                unique[k] = v
+        self.repeated = repeated
         self.unique = unique
     
-    def __get_collapsed(self):
-        collapsed_mixed = {(k, v[0], v[1]) for k,v in self.mixed.items()}
-        collapsed_pure = {(k, v[0], v[1]) for k,v in self.pure.items()}
-        collapsed_unique = {(k, v[0], v[1]) for k,v in self.unique.items()}
+    def __unpack(self):
+        # extracts the data matrix and generates a report of instances belonging to each sequence
+        seq_list = []
+        inst_list = []
+        for k,v in self.unique.items():
+            seq_list.append(k)
+            inst_list.append([list(v), 'unique'])
+        for k,v in self.repeated.items():
+            seq_list.append(k)
+            inst_list.append([list(v), 'repeated'])
         
-        union = collapsed_mixed.union(collapsed_pure).union(collapsed_unique)
-        self.collapsed = pd.DataFrame(union, columns = ['seq', 'tax', 'id'])
+        self.matrix = np.array(seq_list)
+        self.report = pd.DataFrame(inst_list, columns = ['instances', 'status'])
+        self.__get_accs()
+    
+    def __get_accs(self):
+        # add accession codes to the report
+        acc_list = []
+        for _, item in self.report['instances'].iteritems():
+            accs = self.tax_tab.index[item].tolist()
+            acc_list.append(accs)
+        self.report['accs'] = pd.Series(acc_list)
+
+    def __uniq_taxes(self):
+        # extract taxonomic codes of unique sequences
+        idx = [v[0] for v in self.unique.values()]
+        self.taxes_unique = self.tax_tab.iloc[idx,1:].reset_index(drop = True)
+    
+    def __mixd_taxes(self):
+        # exrtract taxonomic codes of repeated sequences
+        repeated = self.report.loc[self.report['status'] == 'repeated', 'instances']
+        taxes_repeated = pd.DataFrame(index = repeated.index, columns = self.taxes_unique.columns)
+        for idx, instances in repeated.iteritems():
+            prev_col = 0
+            for col in taxes_repeated.columns:
+                taxes = self.tax_tab[col].iloc[instances].unique()
+                if len(taxes) == 1:
+                    prev_col = taxes[0]
+                taxes_repeated.at[idx, col] = prev_col
+        self.taxes_repeated = taxes_repeated
+
+    def __build_tax_tab(self):
+        self.tax_collapsed = pd.concat([self.taxes_unique, self.taxes_repeated])
+#%% test - delete
+tax_file = '/home/hernan/PROYECTOS/Graboid/Databases/12_11_2021-23_15_53/Taxonomy_files/Nematoda_18S_tax.tsv'
+mat_file = 'Dataset/12_11_2021-23_15_53/Matrices/Nematoda/18S/17_aln_256-355_n12260.mat'
+
+wc = WindowCollapser(tax_file)
+wc.set_matrix(mat_file)
+wc.collapse()

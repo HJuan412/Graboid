2c3fc0ddeb863ffd0b21e4ecb488b1258289c05a
HJuan412 <hernan.juan@gmail.com>
2022-03-30 16:45:22 -0300

    Ref_catalog and ref_data, tell blaster where to look for the reference database

---
+++ data_fetch/dataset_construction/ref_catalog.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 15:36:34 2022
+
+@author: hernan
+This script is used to help locate the blast databases
+"""
+
+catalog = {'18S':'data_fetch/dataset_construction/ref_data/ref_bases/ref_18S_base/',
+           '28S':'data_fetch/dataset_construction/ref_data/ref_bases/ref_28S_base/',
+           'COI':'data_fetch/dataset_construction/ref_data/ref_bases/ref_COI_base/'}

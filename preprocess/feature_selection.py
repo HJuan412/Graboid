5dcfb9e25eb72867c884f89cff79952c337d4dc2
HJuan412 <hernan.juan@gmail.com>
2022-05-02 16:31:54 -0300

    Modifyed Selector class, now selected_seqs/sites are sored per rank

--- preprocess/feature_selection.py
+++ preprocess/feature_selection.py
@@ -122,65 +122,70 @@ class Selector():
     def __init__(self, matrix, tax):
         self.matrix = matrix
         self.tax = tax
-        self.rank = None
-        self.diff_tab = None
+        self.ranks = tax.columns.tolist()
+        self.diff_tabs = {rk:np.empty(0) for rk in self.ranks}
         # default selection is all the sites and sequences
-        self.selected_tax = None
-        self.selected_seqs = np.arange(matrix.shape[0])
-        self.selected_sites = np.arange(matrix.shape[1])
-    
-    def set_rank(self, rank):
-        # do NOT include the _id suffix
-        # TODo: kill the _id suffix in the taxid table (i hate it)
-        self.rank = rank
+        self.selected_tax = {rk:np.empty(0) for rk in self.ranks}
+        self.selected_seqs = {rk:np.arange(matrix.shape[0]) for rk in self.ranks}
+        self.selected_sites = {rk:np.arange(matrix.shape[1]) for rk in self.ranks}
+        # TODO: kill the _id suffix in the taxid table (i hate it)
 
     def generate_diff_tab(self):
         # Get the entropy difference for each base/taxon/rank
-        sub_mat = self.matrix[self.selected_seqs]
-        sub_tax = self.tax.iloc[self.selected_seqs]
-
-        general_entropy = get_matrix_entropy(sub_mat)
-        p_tax_ent = per_tax_entropy(sub_mat, sub_tax)
-        p_tax_ent = p_tax_ent.loc[p_tax_ent.index.notnull()]
-        diff_tab = p_tax_ent.iloc[:,:-1] - general_entropy
-        diff_tab['rank'] = p_tax_ent['rank']
-        
-        self.diff_tab = diff_tab
+        for rank in self.ranks:
+            sub_mat = self.matrix[self.selected_seqs[rank]]
+            sub_tax = self.tax.iloc[self.selected_seqs[rank]]
+    
+            general_entropy = get_matrix_entropy(sub_mat)
+            p_tax_ent = per_tax_entropy(sub_mat, sub_tax)
+            p_tax_ent = p_tax_ent.loc[p_tax_ent.index.notnull()]
+            diff_tab = p_tax_ent.iloc[:,:-1] - general_entropy
+            diff_tab['rank'] = p_tax_ent['rank']
+            
+            self.diff_tabs[rank] = diff_tab
     
     def select_sites(self, nsites):
         # get the nsites more informative sites per taxon for the current rank
         # must run this AFTER generate diff_tab
         # should run this after select_taxons
-        selected = set()
-        sub_tab = self.diff_tab.loc[self.diff_tab['rank'] == f'{self.rank}_id'].drop('rank', axis = 1)
-        for tax, row in sub_tab.iterrows():
-            sorted_diff = row.sort_values(ascending = False).index.tolist()
-            selected.update(sorted_diff[:nsites])
-        self.selected_sites = np.array(list(selected), dtype = int)
+        for rank in self.ranks:
+            selected = set()
+            diff_tab = self.diff_tabs[rank]
+            sub_tab = diff_tab.loc[diff_tab['rank'] == f'{rank}'].drop('rank', axis = 1)
+            for tax, row in sub_tab.iterrows():
+                sorted_diff = row.sort_values(ascending = False).index.tolist()
+                selected.update(sorted_diff[:nsites])
+            self.selected_sites[rank] = np.array(list(selected), dtype = int)
     
     def select_taxons(self, ntaxes=None, minseqs=None, thresh=None):
         # ntaxes: select the ntaxes most populated taxons
         # minseqs (int): select all taxes with more than minseqs sequences
         # thresh (float): select taxes representing more than thresh percent of the total
         # get the sequences corresponding to the m most populated taxons
-        rank_col = self.tax[f'{self.rank}_id']
-        tax_counts = rank_col.value_counts(ascending = False)
         
-        # can use different criteria for selecting taxons
-        if not ntaxes is None:
-            selected = tax_counts.iloc[:ntaxes]
-        elif not minseqs is None:
-            selected = tax_counts.loc[tax_counts >= minseqs]
-        elif not thresh is None:
-            seq_thresh = len(rank_col) * thresh
-            selected = tax_counts.loc[tax_counts >= seq_thresh]
-        
-        selected_tax = selected.index.to_numpy()
-        selected_idx = np.argwhere(rank_col.isin(selected_tax).values).flatten()
-        self.selected_tax = selected_tax.astype(int)
-        self.selected_seqs = selected_idx.astype(int)
+        for rank in self.ranks:
+            rank_col = self.tax[f'{rank}']
+            tax_counts = rank_col.value_counts(ascending = False)
+            
+            # can use different criteria for selecting taxons
+            if not ntaxes is None:
+                selected = tax_counts.iloc[:ntaxes]
+            elif not minseqs is None:
+                selected = tax_counts.loc[tax_counts >= minseqs]
+            elif not thresh is None:
+                seq_thresh = len(rank_col) * thresh
+                selected = tax_counts.loc[tax_counts >= seq_thresh]
+            
+            selected_tax = selected.index.to_numpy()
+            selected_idx = np.argwhere(rank_col.isin(selected_tax).values).flatten()
+            self.selected_tax[rank] = selected_tax.astype(int)
+            self.selected_seqs[rank] = selected_idx.astype(int)
     
-    def get_training_data(self):
-        selected_matrix = self.matrix[self.selected_seqs][:,self.selected_sites]
-        selected_tax = self.tax.iloc[self.selected_seqs]
+    def get_training_data(self, rank):
+        rank = f'{rank}_id' # TODO: DELETE "_id"
+        row_idxs = self.selected_seqs[rank]
+        col_idxs = self.selected_sites[rank]
+
+        selected_matrix = self.matrix[row_idxs][:, col_idxs]
+        selected_tax = self.tax.iloc[row_idxs]
         return selected_matrix, selected_tax

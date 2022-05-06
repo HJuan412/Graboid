87ee7a14a7887b81a15427e3ad36d7e9d085841d
HJuan412 <hernan.juan@gmail.com>
2022-05-06 16:13:21 -0300

    created the preprocess function

--- preprocess/preproc.py
+++ preprocess/preproc.py
@@ -16,82 +16,90 @@ from preprocess import windows
 from preprocess import feature_selection as fsel
 from preprocess import taxon_study as tstud
 import numpy as np
-import pandas as pd
-import time
-#%%
-taxon = 'nematoda'
-marker = '18s'
-in_dir = '/home/hernan/PROYECTOS/nematoda_18s/out_dir'
-out_dir = '/home/hernan/PROYECTOS/nematoda_18s/out_dir'
-tmp_dir = '/home/hernan/PROYECTOS/nematoda_18s/tmp_dir'
-warn_dir = '/home/hernan/PROYECTOS/nematoda_18s/warn_dir'
 
-max_pos = 1730
-w_len = 200
-row_thresh = 0.2
-col_thresh = 0.2
-
-cost_mat = cost_matrix.cost_matrix()
-id_mat = cost_matrix.id_matrix()
-
-#%% cycle trough rank and windows
-step = 15
-
-rang = np.arange(0, max_pos - w_len, step)
-if rang[-1] < max_pos - w_len:
-    np.append(rang, max_pos - w_len)
-ranks = ('phylum', 'class', 'order', 'family', 'genus', 'species')
-if __name__ == '__main__':
-    # generate window
-    wl = windows.WindowLoader(taxon, marker, in_dir, out_dir, tmp_dir, warn_dir)
-    # TODO: handle warning when window doesn't pass one of the thresholds
-    for w_start in rang:
-        w_end = w_start + w_len
-        window = wl.get_window(w_start, w_end, row_thresh, col_thresh)
-        
-        for rank in ranks:
-            # select attributes
-            if len(window.cons_mat > 1):
-                selector = fsel.Selector(window.cons_mat, window.cons_tax)
-                selector.set_rank(rank)
-                
-                selector.select_taxons(minseqs = 10)
-                selector.generate_diff_tab()
-                selector.select_sites(10)
-                t_mat, t_tax = selector.get_training_data()
-                
-                if len(t_mat) > 1:
-                    super_c = tstud.SuperCluster(t_mat, t_tax, rank)
-                    col_tax, col_mat = super_c.get_collapsed_data()
-
-#%% single execution
-w_start = 250
-rank = 'family'
-if __name__ == '__main__':
-    # generate window
-    wl = windows.WindowLoader(taxon, marker, in_dir, out_dir, tmp_dir, warn_dir)
-    # TODO: handle warning when window doesn't pass one of the thresholds
-    w_end = w_start + w_len
-    w0 = np.array(wl.matrix[:,w_start:w_end])
-    rows = windows.filter_matrix(w0, 0.2)
-    w1 = w0[rows]
-    cols = windows.filter_matrix(w1, 0.2, 1)
-    window = w1[:,cols]
-    effs = windows.get_effective_seqs(window)
-    #%%
-    # pre collapse happens in window construction
-    window = wl.get_window(w_start, w_end, row_thresh, col_thresh)
+#%% functions
+def preprocess(window, row_thresh=0.2, col_trhesh=0.2, minseqs=5, nsites=10, rank='genus'):
     # select attributes
     if len(window.cons_mat > 1):
         selector = fsel.Selector(window.cons_mat, window.cons_tax)
-        selector.set_rank(rank)
-        
-        selector.select_taxons(minseqs = 10)
+        selector.select_taxons(minseqs=minseqs)
         selector.generate_diff_tab()
-        selector.select_sites(10)
-        t_mat, t_tax = selector.get_training_data()
+        selector.select_sites(nsites)
+        t_mat, t_tax = selector.get_training_data(rank)
         # post collapse, generates the final data matrix and taxonomy table
         x, y = windows.collapse(t_mat, t_tax.reset_index())
         if len(t_mat) > 1:
-            super_c = tstud.SuperCluster(t_mat, t_tax, rank)
-            col_tax, col_mat = super_c.get_collapsed_data()
+            super_c = tstud.SuperCluster(x, y, rank)
+    return x, y, super_c
+
+#%%
+if __name__ == '__main__':
+    taxon = 'nematoda'
+    marker = '18s'
+    in_dir = '/home/hernan/PROYECTOS/nematoda_18s/out_dir'
+    out_dir = '/home/hernan/PROYECTOS/nematoda_18s/out_dir'
+    tmp_dir = '/home/hernan/PROYECTOS/nematoda_18s/tmp_dir'
+    warn_dir = '/home/hernan/PROYECTOS/nematoda_18s/warn_dir'
+    
+    max_pos = 1730
+    w_len = 200
+    row_thresh = 0.2
+    col_thresh = 0.2
+    
+    cost_mat = cost_matrix.cost_matrix()
+    id_mat = cost_matrix.id_matrix()
+    
+    #%% cycle trough rank and windows
+    step = 15
+    
+    rang = np.arange(0, max_pos - w_len, step)
+    if rang[-1] < max_pos - w_len:
+        np.append(rang, max_pos - w_len)
+    ranks = ('phylum', 'class', 'order', 'family', 'genus', 'species')
+    if __name__ == '__main__':
+        # generate window
+        wl = windows.WindowLoader(taxon, marker, in_dir, out_dir, tmp_dir, warn_dir)
+        # TODO: handle warning when window doesn't pass one of the thresholds
+        for w_start in rang:
+            w_end = w_start + w_len
+            window = wl.get_window(w_start, w_end, row_thresh, col_thresh)
+            
+            for rank in ranks:
+                # select attributes
+                if len(window.cons_mat > 1):
+                    selector = fsel.Selector(window.cons_mat, window.cons_tax)
+                    selector.set_rank(rank)
+                    
+                    selector.select_taxons(minseqs = 10)
+                    selector.generate_diff_tab()
+                    selector.select_sites(10)
+                    t_mat, t_tax = selector.get_training_data()
+                    
+                    if len(t_mat) > 1:
+                        super_c = tstud.SuperCluster(t_mat, t_tax, rank)
+                        col_tax, col_mat = super_c.get_collapsed_data()
+    
+    #%% single execution
+    w_start = 250
+    rank = 'family'
+    if __name__ == '__main__':
+        # generate window
+        wl = windows.WindowLoader(taxon, marker, in_dir, out_dir, tmp_dir, warn_dir)
+        # TODO: handle warning when window doesn't pass one of the thresholds
+    
+        # pre collapse happens in window construction
+        window = wl.get_window(w_start, w_end, row_thresh, col_thresh)
+        # select attributes
+        if len(window.cons_mat > 1):
+            selector = fsel.Selector(window.cons_mat, window.cons_tax)
+            # selector.set_rank(rank)
+            
+            selector.select_taxons(minseqs = 5)
+            selector.generate_diff_tab()
+            selector.select_sites(10)
+            t_mat, t_tax = selector.get_training_data('genus')
+            # post collapse, generates the final data matrix and taxonomy table
+            x, y = windows.collapse(t_mat, t_tax.reset_index())
+            if len(t_mat) > 1:
+                super_c = tstud.SuperCluster(x, y, 'genus')
+                col_tax, col_mat = super_c.get_collapsed_data()

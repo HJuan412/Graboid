fe500d70613d122ea3c7a5829788332702eeaa9a
Hernán Juan <hernan.juan@gmail.com>
2022-09-01 13:06:20 -0300

    fixed docs_preprocessing

--- preprocess/taxon_study.py
+++ preprocess/taxon_study.py
@@ -7,30 +7,22 @@ Created on Mon Apr  4 16:00:03 2022
 Study the internal variation of a taxonomic cluster of sequences
 """
 
-#%%
-import sys
-sys.path.append('classif')
-sys.path.append('preprocess')
 #%% libraries
-import cost_matrix
-import distance
+from classif import classification
 import numpy as np
-import windows
-#%% vars
-cost = cost_matrix.cost_matrix()
+
 #%% functions
-def build_tax_clusters(matrix, tax_tab, rank, dist_matrix=cost):
+def build_tax_clusters(matrix, tax_array, dist_mat):
     # generates a dictionary of the form taxid:TaxCluster, also returns a list of the unique taxids
     clusters = {}
-    taxes = tax_tab[rank].to_numpy()
     # build clusters
-    for tax in np.unique(taxes):
-        sub_matrix = matrix[taxes == tax]
-        clusters[tax] = TaxCluster(sub_matrix, tax, rank, dist_matrix)
+    for tax in np.unique(tax_array):
+        sub_matrix = matrix[tax_array == tax]
+        clusters[tax] = TaxCluster(sub_matrix, dist_mat)
     
     return clusters
 
-def get_paired_dists(matrix, dist_matrix=cost):
+def get_paired_dists(matrix, dist_mat):
     # constructs a paired distance matrix of the given matrix using a distance matrix
     # distance data is located on the top right of the matrix
     nseqs = matrix.shape[0]
@@ -38,7 +30,7 @@ def get_paired_dists(matrix, dist_matrix=cost):
     for idx0, seq0 in enumerate(matrix):
         for idx1 in range(idx0 + 1, matrix.shape[0]):
             seq1 = matrix[idx1]
-            paired[idx0, idx1] = distance.calc_distance(seq0, seq1, dist_matrix)
+            paired[idx0, idx1] = classification.calc_distance(seq0, seq1, dist_mat)
     return paired
 
 def get_flat_dists(matrix):
@@ -51,8 +43,8 @@ def get_flat_dists(matrix):
 
 def get_rowcol(mat, idx):
     # get the column and row values for the given index of a distance matrix
-    col = mat[:idx + 1, idx + 1].flatten()
-    row = mat[idx, idx+2:].flatten()
+    col = mat[:idx,idx]
+    row = mat[idx, idx+1:]
     return np.concatenate((col, row))
 
 #%% classes
@@ -72,49 +64,42 @@ class clust_iterator:
 
 class SuperCluster:
     # This class contains all the taxon clusters of selected data
-    def __init__(self, matrix, tax_tab, rank, dist_matrix=cost):
-        self.clusters = build_tax_clusters(matrix, tax_tab, rank, cost)
-        self.tax_list = list(self.clusters)
-        self.centroids = {tax:cluster.centroid for tax, cluster in self.clusters.items()}
-        self.centroid_dists = get_paired_dists(self.centroids, dist_matrix)
-        self.get_collisions()
+    def __init__(self, matrix, tax_tab, dist_mat):
+        tax_mat = tax_tab.to_numpy().T
+        self.clusters = {}
+        for rk_idx, tax_array in enumerate(tax_mat):
+            self.clusters[rk_idx] = build_tax_clusters(matrix, tax_array, dist_mat)
+        # self.centroids = {rank:{tax:cluster.centroid for tax, cluster in clusters.items()} for rank, clusters in self.clusters.items()}
+        # self.centroid_dists = get_paired_dists(self.centroids, dist_mat)
+        # self.get_collisions()
     
     def __getitem__(self, item):
+        rk, tax = item
         return self.clusters[item]
-    
-    def __iter__(self):
-        return clust_iterator(self)
 
 class TaxCluster:
     # this class holds the sequences of a unique taxon
     # used to generate relevant data (paired distance, dispersion, centroids) and collapse unique sequences at the taxon level
-    def __init__(self, matrix, taxid, rank='genus', dist_matrix=cost):
-        self.matrix = matrix
-        self.paired = get_paired_dists(matrix, dist_matrix)
-        self.taxid = taxid
-        self.rank = rank
+    def __init__(self, matrix, dist_mat):
+        paired = get_paired_dists(matrix, dist_mat)
         self.nseqs = matrix.shape[0]
-        self.get_params()
-    
-    def get_mean_dists(self):
-        means = []
-        for idx in range(self.nseqs -1):
-            dists = get_rowcol(self.paired, idx)
-            means.append(dists.mean())
-        self.means = means
-        self.centroid = self.matrix[np.argmin(means)]
-
-    def get_params(self):
         if self.nseqs == 1:
             self.mean = 0
             self.std = 0
             self.max_range = 0
             self.means = [0]
-            self.centroid = self.matrix[0]
+            self.centroid = matrix[0]
         else:
-            flat_distances = get_flat_dists(self.paired)
+            flat_distances = get_flat_dists(paired)
             self.mean = flat_distances.mean()
             self.std = flat_distances.std()
             self.max_range = flat_distances.max()
-            self.get_mean_dists()
-    
+            # get mean distance from every member of the cluster to everyone else
+            # set centroid as the member with the LOWEST mean distance
+            means = []
+            for idx in np.arange(self.nseqs):
+                dists = get_rowcol(paired, idx)
+                means.append(dists.mean())
+            self.means = means
+            self.centroid = matrix[np.argmin(means)]
+            self.dists_to_centroid = get_rowcol(paired, np.argmin(means))

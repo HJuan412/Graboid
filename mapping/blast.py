c94012a37e80e70359169bf5570c17c5f7752272
Hernán Juan <hernan.juan@gmail.com>
2022-12-19 12:56:18 -0300

    removed make_ref_db from Blaster class (now handled by the director)

--- mapping/blast.py
+++ mapping/blast.py
@@ -13,6 +13,7 @@ from Bio.Blast.Applications import NcbimakeblastdbCommandline as makeblast_cline
 from glob import glob
 import logging
 import os
+import re
 
 #%% set logger
 logger = logging.getLogger('Graboid.mapper.blast')
@@ -40,6 +41,7 @@ def makeblastdb(ref_file, db_prefix):
     cline()
 
 def check_db_dir(db_dir):
+    # counts the database files present at the given location, check returns True if six .n* files are found
     db_files = glob(f'{db_dir}/*.n*')
     check = False
     if len(db_files) == 6:
@@ -51,36 +53,23 @@ class Blaster:
     def __init__(self, out_dir):
         self.report = None
         self.out_dir = out_dir
-
-    def make_ref_db(self, ref_file, db_dir, clear=False):
-        check, db_files = check_db_dir(db_dir)
-        if clear or not check:
-            # found db files are incomplete or option 'clear' is enabled
-            # delete previous files and create new databases
-            for file in db_files:
-                os.remove(file)
-            makeblastdb(ref_file, f'{db_dir}/ref')
-            logger.info(f'Generated blast reference databse at directory {db_dir}')
-            return
-        logger.warning(f'Directory {db_dir} already contains a database. Set \'clear\' to False if you wish to replace it.')
     
     def blast(self, fasta_file, db_dir, out_name=None, threads=1):
+        self.report = None
         check, db_files = check_db_dir(db_dir)
         if not check:
             logger.error(f'Incomplete BLAST database ({len(db_files)} files found)')
             return
-        db_prefix = db_files[0].split('.n')[0]
         
         # set output name
-        if not out_name is None:
-            blast_out = f'{self.out_dir}/{out_name}.BLAST'
-        else:
-            blast_out = fasta_file.split('/')[-1].split('.')[0]
-            blast_out = f'{self.out_dir}/{blast_out}.BLAST'
+        if out_name is None:
+            out_name = re.sub('.*/', '', re.sub('\..*', '', fasta_file))
+        blast_out = f'{self.out_dir}/{out_name}.BLAST'
+        
         # perform BLAST
         print(f'Blasting {fasta_file}')
         try:
-            blast(fasta_file, db_prefix, blast_out, threads)
+            blast(fasta_file, db_dir, blast_out, threads)
             self.report = blast_out
             logger.info(f'Generated blast report at {blast_out}')
         except:

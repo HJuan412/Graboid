1d81676fc4bc750e48c3a9af98d2806477448969
Hernán Juan <hernan.juan@gmail.com>
2023-04-24 16:59:10 -0300

    Added plotting functions. TODO: add option to exclude null and 0 score rows

--- calibration/calibrator.py
+++ calibration/calibrator.py
@@ -10,10 +10,13 @@ Created on Tue Nov 30 11:06:10 2021
 import concurrent.futures
 import json
 import logging
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 import numba as nb
 import numpy as np
 import os
 import pandas as pd
+import seaborn as sns
 import sys
 import time
 
@@ -617,6 +620,92 @@ def translate_params(params, n_range, k_range, methods='uwd'):
             # update the corresponding cell
             param_datum[rk_idx][pos0, pos1] = (n,k,m)
     return param_datum
+
+# plotting
+def plot_results(data,
+                 param_annots,
+                 title,
+                 metric,
+                 cell_size=45,
+                 dpi=300,
+                 height_pad=5,
+                 ratio_pad=10,
+                 hspace=0.01):
+    # set color map
+    viridis = cm.get_cmap('viridis', 256)
+    grey = np.array([128/256, 128/256, 128/256, 1])
+    viridis.set_bad(grey)
+    
+    # calculate heatmap and figure dimensions
+    map_width = data.shape[1] * cell_size
+    map_height = (data.shape[0] + height_pad) * cell_size # height pad is used to compensate for the space taken by the title and window labels
+    fig_width = map_width / dpi
+    fig_height = map_height / dpi
+    
+    # ensure the figure does not exceed pyplot maximum size (2**16 px)
+    ratio = 2**16 / map_height
+    annot_size = 2
+    ytick_size = 4
+    xtick_size = 3
+    hm_lab_size = 5
+    if ratio < 1:
+        # picture would exceed maximum allowed size, lower resolution
+        ratio *= 0.999 # figure should be just below the maximum allowed size
+        dpi *= ratio
+        # increase font size of labels and annotations
+        annot_size /= (ratio * 1.1) # don't go overboard with annotation size increase
+        ytick_size /= (ratio * 1.1)
+        xtick_size /= (ratio * 1.1)
+        hm_lab_size /= (ratio * 1.1)
+        
+    # initialize figure
+    fig = plt.figure(figsize=(fig_width, fig_height), dpi=dpi)
+    gs = fig.add_gridspec(nrows=2, ncols=1, height_ratios = [data.shape[0] + ratio_pad, 1], hspace = hspace) # use this to get a fine control over the colorbar position and size
+    # top ax contains the heatmap, bottom ax contains the colorbar
+    ax_hm = fig.add_subplot(gs[0])
+    ax_cb = fig.add_subplot(gs[1])
+    
+    # build heatmap
+    hm = sns.heatmap(data,
+                     square = True,
+                     cmap = viridis,
+                     annot = param_annots,
+                     fmt='',
+                     annot_kws={"size": annot_size, 'ha':'center', 'va':'center'},
+                     ax = ax_hm,
+                     cbar_kws = {'orientation':'horizontal'},
+                     cbar_ax = ax_cb,
+                     vmin=0,
+                     vmax=1)
+    
+    # place labels for all windows and taxa
+    hm.set_yticks(np.arange(data.shape[0]) + 0.5)
+    hm.set_yticklabels(data.index, size = ytick_size, rotation = 30, va='top')
+    hm.set_xticks(np.arange(data.shape[1]) + 0.5)
+    hm.set_xticklabels(data.columns, size = xtick_size, rotation=60, ha='left')
+    # relocate window labels to the top of the plot
+    ax_hm.xaxis.tick_top()
+    ax_hm.xaxis.set_label_position('top')
+    # name axes
+    ax_hm.set_ylabel('Taxa', size=hm_lab_size)
+    ax_hm.set_xlabel('Windows', size=hm_lab_size)
+    
+    # adjust colorbar labels
+    ax_cb.tick_params(labelsize = 4)
+    ax_cb.set_xlabel(metric, size = 4)
+    ax_hm.set_title(title, size=5)
+    
+    return fig
+
+def build_annot(params):
+    # this function generates an annotations array for a given rank's report
+    def get_annot(cell):
+        if isinstance(cell, tuple):
+            return '%d\n%d\n%s' % cell
+        return ''
+    transform = np.vectorize(get_annot)
+    annot = transform(params)
+    return annot
 #%% classes
 class Calibrator:
     def __init__(self, out_dir, warn_dir, prefix='calibration'):
@@ -852,6 +941,22 @@ class Calibrator:
         params = translate_params(params, n_range, k_range)
         t7 = time.time()
         print(f'Done in {t7 - t6:.3f} seconds')
+        
+        # plot results
+        print('Plotting results...')
+        
+        for rk_idx, rk in enumerate(self.tax_ext.columns[:-4]):
+            rk_report = pre_report.loc[rk]
+            sort_taxa = np.argsort(rk_report.index)
+            rk_report = rk_report.sort_index()
+            rk_params = params[rk_idx][sort_taxa]
+            rk_annot = build_annot(rk_params)
+            
+            title = f'{metric} scores for rank {rk}nN = [{min_n}:{max_n}:{step_n}]\nK = [{min_k}:{max_k}:{step_k}]\nu = unweighted, w = wKNN, d = dwKNN'
+            fig = plot_results(rk_report, rk_annot, title, metric)
+            fig.savefig(self.out_dir + f'/{rk}.png', format='png', bbox_inches='tight')
+        t8 = time.time()
+        print(f'Done in {t8 - t7:.3f} seconds')
         print(f'Finished in {t7 - t0:.3f} seconds')
         return pre_report, params
             

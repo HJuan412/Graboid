41712d248332874badba1c1558e47d458178ea84
Hernán Juan <hernan.juan@gmail.com>
2023-04-28 14:36:32 -0300

    updated plot_results with collapse argument

--- calibration/cal_plot.py
+++ calibration/cal_plot.py
@@ -89,24 +89,46 @@ def build_annot(params):
     annot = transform(params)
     return annot
 
-def plot_results(report, params, metric, prefix, ranks):
+def collapse_report(report):
+    # remove rows that have all null or 0 values
+    max_vals = report.max(axis=1)
+    filtered_taxa = max_vals.loc[max_vals > 0].index
+    rejected_taxa = max_vals.loc[max_vals == 0].index.to_numpy()
+    collapsed = report.loc[filtered_taxa]
+    return collapsed, rejected_taxa
+
+def plot_results(report, params, metric, prefix, ranks, collapse=True):
+    # collapse eliminates taxa with no values over 0 to reduce heatmap size, if set to True, a file of rejected taxa is generated
+    collapse_dict = {rk:[] for rk in ranks}
     for rk_idx, rk in enumerate(ranks):
         rk_report = report.loc[rk]
         sort_taxa = np.argsort(rk_report.index)
         rk_report = rk_report.sort_index()
         rk_params = params[rk_idx][sort_taxa]
+        if collapse:
+            rk_idx = rk_report.index.to_numpy()
+            rk_report, rk_rejected = collapse_report(rk_report)
+            rk_params = rk_params[(~np.isin(rk_idx, rk_rejected))]
+            collapse_dict[rk] = rk_rejected
         rk_annot = build_annot(rk_params)
-        
-        title = f'{metric} scores for rank {rk}\nN\nK\nmethod (u = unweighted, w = wKNN, d = dwKNN)'
+        title = f'{metric} scores for rank: {rk}\nN\nK\nmethod (u = unweighted, w = wKNN, d = dwKNN)'
         
         # heatmaps have at most 1000 rows, if a rank (usually only species, MAYBE genus) exceeds the limit, split it
         if len(rk_report) <= 1000:
             fig = build_heatmap(rk_report, rk_annot, title, metric)
-            fig.savefig(prefix + f'{rk}.png', format='png', bbox_inches='tight')
+            fig.savefig(prefix + f'/{metric}_{rk}.png', format='png', bbox_inches='tight')
             plt.close()
         else:
             for n_subplot, subplot_idx in enumerate(np.arange(0, len(rk_report), 1000)):
                 fig = build_heatmap(rk_report.iloc[subplot_idx: subplot_idx + 1000], rk_annot[subplot_idx: subplot_idx + 1000], title, metric)
-                fig.savefig(prefix + f'{rk}.{n_subplot + 1}.png', format='png', bbox_inches='tight')
+                fig.savefig(prefix + f'/{metric}_{rk}.{n_subplot + 1}.png', format='png', bbox_inches='tight')
                 plt.close()
-            
+    if collapse:
+        with open(prefix + f'/{metric}_rejected.txt', 'w') as handle:
+            handle.write('The following taxa yielded no {metric} scores over 0 and were ommited from the plots:\n')
+            for rk in ranks:
+                if len(collapse_dict[rk]) == 0:
+                    continue
+                handle.write(f'{rk}:\n')
+                for tax in collapse_dict[rk]:
+                    handle.write(f'\t{tax}\n')

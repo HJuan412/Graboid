fixed issue with metrics calculation, tested calibration report

calibration/cal_calibrator.py
@@ -348,12 +348,12 @@ class Calibrator:
         # get metrics
         print('Calculating metrics...')
         win_dict = {w_idx:win for w_idx,win in zip(win_indexes, win_list)} # use this to locate the right window from the file's parameters
-        for res_file in os.listdir(self.clasi_dir):
-            results = np.load(self.clasif_dir + '/' +res_file)
+        for res_file in os.listdir(self.classif_dir):
+            results = np.load(self.classif_dir + '/' +res_file)
             window = win_dict[results['params'][0]]
-            real_tax = self.tax_ext.loc[window.taxonomy].to_numpy()
-            metrics, cross_entropy = cal_metrics.get_metrics0(results, real_tax)
-            np.savez(self.reports_dir + '/' + re.sub('.npz', '_metrics.npz'), metrics = metrics, cross_entropy = cross_entropy, params = results['params'])
+            real_tax = np.nan_to_num(self.tax_ext.loc[window.taxonomy].to_numpy(), nan=-2) # undetermined taxa in real taxon are marked with -2 to distinguish them from undetermined taxa in predicted
+            metrics, cross_entropy, valid_taxa = cal_metrics.get_metrics0(results, real_tax)
+            np.savez(self.reports_dir + '/' + re.sub('.npz', '_metrics.npz'), metrics = metrics, cross_entropy = cross_entropy, valid_taxa = valid_taxa, params = results['params'])
         t6 = time.time()
         print(f'Done in {t6 - t5:.3f} seconds')
         
@@ -365,15 +365,12 @@ class Calibrator:
         post_process(self.windows, self.guide, self.ranks, met_report=prc_report)
         post_process(self.windows, self.guide, self.ranks, met_report=rec_report)
         post_process(self.windows, self.guide, self.ranks, met_report=f1_report)
-            
-        # acc_report, acc_params = cal_report.build_report(win_list, metrics, 'acc', self.tax_ext, self.guide, n_range, k_range)
-        # prc_report, prc_params = cal_report.build_report(win_list, metrics, 'prc', self.tax_ext, self.guide, n_range, k_range)
-        # rec_report, rec_params = cal_report.build_report(win_list, metrics, 'rec', self.tax_ext, self.guide, n_range, k_range)
-        # f1_report, f1_params = cal_report.build_report(win_list, metrics, 'f1', self.tax_ext, self.guide, n_range, k_range)
-        # self.report_metrics(acc_report, acc_params, 'acc')
-        # self.report_metrics(prc_report, prc_params, 'prc')
-        # self.report_metrics(rec_report, rec_params, 'rec')
-        # self.report_metrics(f1_report, f1_params, 'f1')
+        
+        cross_entropy_report.to_csv(self.reports_dir + '/cross_entropy.csv')
+        acc_report.to_csv(self.reports_dir + '/acc_report.csv')
+        prc_report.to_csv(self.reports_dir + '/prc_report.csv')
+        rec_report.to_csv(self.reports_dir + '/rec_report.csv')
+        f1_report.to_csv(self.reports_dir + '/f1_report.csv')
         t7 = time.time()
         print(f'Done in {t7 - t6:.3f} seconds')
         

calibration/cal_metrics.py
@@ -133,7 +133,7 @@ def get_metrics_per_func(pred, real):
             tp = (real_loc & pred_loc).sum()
             tn = (~real_loc & ~pred_loc).sum()
             fp = pred_loc[~real_loc].sum()
-            fn = ~pred_loc[real_loc].sum()
+            fn = (~pred_loc[real_loc]).sum()
             
             accuracy = tp / (tp + tn + fp + fn)
             if tp == 0:
@@ -148,20 +148,24 @@ def get_metrics_per_func(pred, real):
         metrics.append(rk_metrics)
     return np.concatenate(metrics)
 
-def get_cross_entropy(supports):
-    clipped_supports = np.clip(supports, np.exp(-5), 1)
-    cross_entropy = -np.log(clipped_supports).sum(axis=0)
+def get_cross_entropy(supports, valid):
+    clipped_supports = np.clip(supports, np.exp(-10), 1) # max loss is 10
+    log_supports = -np.log(clipped_supports)
+    cross_entropy = np.array([col[val].sum() for col, val in zip(log_supports.T, valid.T)]) / valid.sum(0)
     return cross_entropy
 
 def get_metrics0(results, real_tax):
     # metrics is a 3d array of shape: # taxa, 6 (rank_id, tax_id, acc, prc, rec, f1), 3 (unweighted, wknn, dwknn)
+    # real_tax is a 2d array with the (REAL) tax ids of the query sequences
     # cross entropy is a 2d array of shape: 3 (unweighted, wknn, dwknn), # ranks
     metrics = np.array([get_metrics_per_func(results['predicted_u'], real_tax).T,
                         get_metrics_per_func(results['predicted_w'], real_tax).T,
                         get_metrics_per_func(results['predicted_d'], real_tax).T]).T
     
+    valid = real_tax != -2 # get the locations of all known classifications
+    total_valid = valid.sum(0) # array of shape #ranks, counts known taxons per rank
     # Cross Entropy Loss
-    cross_entropy = np.array([get_cross_entropy(results['real_u_support']),
-                              get_cross_entropy(results['real_w_support']),
-                              get_cross_entropy(results['real_d_support'])])
-    return metrics, cross_entropy
+    cross_entropy = np.array([get_cross_entropy(results['real_u_support'], valid),
+                              get_cross_entropy(results['real_w_support'], valid),
+                              get_cross_entropy(results['real_d_support'], valid)])
+    return metrics, cross_entropy, total_valid

calibration/cal_report.py
@@ -121,8 +121,8 @@ def compare_metrics(files):
     taxa, tax_loc = np.unique(tax_data[:,1], return_index = True) # get number of taxon and index location to match tax and rank
 
     template_tab = np.zeros((len(taxa), 6, len(params))) # rows: taxa, columns: rank, taxon, acc, prec, rec, f1, layers: param (n-k) combos
-    template_tab[:, 0] = np.tile(tax_data[tax_loc, 0], (20, 1)).T
-    template_tab[:, 1] = np.tile(tax_data[tax_loc, 1], (20, 1)).T
+    template_tab[:, 0] = np.tile(tax_data[tax_loc, 0], (len(params), 1)).T
+    template_tab[:, 1] = np.tile(tax_data[tax_loc, 1], (len(params), 1)).T
     
     idx_dict = {tax:tax_idx for tax_idx,tax in enumerate(taxa)} # used to place a taxon on nk_tab and method_tab
     nk_tab = template_tab.copy() # holds the scores of the winning method for each taxon for each metric, for each param combination
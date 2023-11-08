ae4b0282026992d39510a4f9fba120bf7f1c7b35
Hernán Juan <hernan.juan@gmail.com>
2023-11-08 09:52:09 -0300

    Removed junk functions

--- DATA/DATA.py
+++ DATA/DATA.py
@@ -21,16 +21,10 @@ import os
 
 DATAPATH = os.path.dirname(__file__)
 DBASES = [db_dir for db_dir in os.listdir(DATAPATH) if (os.path.isdir(DATAPATH + '/' + db_dir) and db_dir != '__pycache__')]
-
-def update_maps(maps):
-    with open(DATAPATH + '/maps.json', 'w') as handle:
-        json.dump(maps, handle)
-
-def add_map(database, fasta, map_file, acc_file, evalue=None, dropoff=None, min_height=None, min_width=None):
-    if not database in MAPS.keys():
-        MAPS[database] = {}
-    MAPS[database].update({fasta:{'map':map_file, 'acc':acc_file, 'evalue':evalue, 'dropoff':dropoff, 'min_height':min_height, 'min_width':min_width}})
-    update_maps(MAPS)
+DBASE_INFO = {}
+for dbase in DBASES:
+    with open(DATAPATH + '/' + dbase, 'r') as handle:
+        DBASE_INFO[dbase] = json.load(handle)
 
 def database_exists(database):
     return database in DBASES
@@ -38,37 +32,4 @@ def database_exists(database):
 def get_database(database):
     if database_exists(database):
         return f'{DATAPATH}/{database}'
-    raise Exception(f'Database {database} not found among: {" ".join(DBASE_LIST.keys())}')
-
-def add_calibration(database, fasta, report, meta):
-    MAPS[database][fasta]['cal'] = report
-    MAPS[database][fasta]['cal_meta'] = meta
-    update_maps(MAPS)
-    
-# load dbase descriptions
-DBASE_LIST = {}
-for db in DBASES:
-    try:
-        with open(DATAPATH + '/' + db + '/desc.json', 'r') as handle:
-            db_desc = json.load(handle)
-    except FileNotFoundError:
-        db_desc = ''
-    DBASE_LIST[db] = db_desc
-    
-# load map list
-try:
-    with open(DATAPATH + '/maps.json', 'r') as handle:
-        MAPS = json.load(handle)
-except FileNotFoundError:
-    MAPS = {}
-# check that maps still exist
-for db, fasta_dict in copy.deepcopy(MAPS).items():
-    for fasta, map_files in fasta_dict.items():
-        # if either map or acc file are missing, remove map from list
-        if not os.path.isfile(map_files['map']) or not os.path.isfile(map_files['acc']):
-            del MAPS[db][fasta]
-    if len(MAPS[db]) == 0:
-        del MAPS[db]
-update_maps(MAPS)
-
-# TODO: add function to remove database, should issue warning and ask confirmation
+    raise Exception(f'Database {database} not found among: {" ".join(DBASES)}')

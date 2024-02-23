2387bf994396439c1ef51dd3de0282a03f6d2335
HJuan412 <hernan.juan@gmail.com>
2024-02-23 17:01:06 -0300

    Defined functions

--- database/db_from_fasta.py
+++ database/db_from_fasta.py
@@ -57,7 +57,6 @@ def unfold_lineage(taxid, node_tab, *ranks):
         Missing ranks are not included
 
     """
-    ranks = ranks[0]
     lineage = {}
     while len(lineage) < len(ranks) and node_tab.loc[taxid, 'Parent'] != 1:
         rk = node_tab.loc[taxid, 'Rank']
@@ -93,45 +92,119 @@ def retrieve_lineages(taxids, node_tab, *ranks):
         lineages.loc[ln] = unfold_lineage(ln, node_tab, *ranks)
     return lineages
 
-#%%
+#%% load taxonomy data
 names_tab = pd.read_csv('test/names.tsv', sep='\t', header=None, index_col=0, names='sciName TaxId'.split())
 names_tab.loc[0] = 'unknown'
 nodes_tab = pd.read_csv('test/nodes.tsv', sep='\t', header=None, index_col=0, names='TaxId Parent Rank'.split())
-tax_file = '/media/hernan/disco_viejo/PROYECTOS/Maestria/Silva/arb-silva.de_2024-01-22_id1299351/headers.csv'
-sep=';'
-records = []
-with open(tax_file, 'r') as h:
-    for row in h.read().splitlines():
-        # get last 3 taxa from each record, should be enough to establish a valid lineage
-        cols = row.split(sep)
-        records.append([cols[0]] + cols[-3:])
-tax_tab = pd.DataFrame(records).set_index(0)
 
-lowest_taxa = pd.Series('', index=tax_tab.index)
-for col in tax_tab.columns:
-    lowest_taxa.loc[tax_tab[col].isin(names_tab.TaxId.values)] = tax_tab[col]
-taxids = names_tab.reset_index().set_index('TaxId').loc[lowest_taxa.values].set_index(tax_tab.index).sciName
+def detect_separator(line):
+    """
+    Detect separator characters from the source taxonomy table. Expect it to be
+    either ',', ';', '\t'. Raise exception if none of those was used.
+
+    """
+    # criterion, separator should be one of these characters, splitting a line usingg the incorrect separator should return a 1 element lise
+    seps = [',', ';', '\t']
+    separator = ''
+    max_len = 0
+    for s in seps:
+        if len(line.split(s)) > max_len:
+            separator = s
+            max_len = len(line.split(s))
+    if max_len == 1:
+        raise Exception("Can't recognize separator character. Be kind and ensure the taxonomy table uses one of ',', ';', or '\t' as separator character]")
+    return separator
+    
+def parse_source_tax(tax_file):
+    """
+    Load the provided taxonomy table. Get the last three taxa for each record.
+    Assume the first column corresponds to the sequence accession.
+
+    Parameters
+    ----------
+    tax_file : str
+        Path to the taxonomy file.
+
+    Returns
+    -------
+    source_tax : pandas.DataFrame
+        Dataframe with index : accession codes. 3 columns, last 3 taxa for each record
+
+    """
+    records = []
+    with open(tax_file, 'r') as src:
+        lines = src.read().splitlines()
+        sep = detect_separator(lines[0])
+        for line in lines:
+            cols = line.split(sep)
+            # select accession & last 3 taxa
+            records.append([cols[0]] + cols[-3:])
+    source_tax = pd.DataFrame(records).set_index(0)
+    return source_tax
+
+def get_acc_taxids(src_tax, names_tab):
+    # select the lowest valid taxon for each record
+    lowest_taxa = pd.Series('', index=src_tax.index)
+    for idx, col in src_tax.T.iterrows():
+        lowest_taxa.loc[col.isin(names_tab.TaxId)] = col
+    # assign TaxId for each record
+    acc_taxids = names_tab.reset_index().set_index('TaxId').loc[lowest_taxa.values].set_index(src_tax.index).sciName
+    return acc_taxids
+
+tax_file = '/home/hernan/PROYECTOS/Maestria/Silva/arb-silva.de_2024-01-22_id1299351/headers.csv'
+source_tax = parse_source_tax(tax_file)
+acc_taxids = get_acc_taxids(source_tax, names_tab)
 #%% build lineage table
+def build_lineage_table(taxid_tab, nodes_tab, *ranks):
+    """
+    Rebuild the lineage for each taxon present in the records.
+    Include lineages of upper taxa
+
+    Parameters
+    ----------
+    taxid_tab : pandas.DataFrame
+        DataFrame containing the 3 last taxa for each record.
+    nodes_tab : pandas.DataFrame
+        DataFrame containing each taxon's parent TaxId and rank.
+    *ranks : str
+        Ranks to be included in the lineage table.
+
+    Returns
+    -------
+    lineages : TYPE
+        DESCRIPTION.
+    real_taxids : TYPE
+        DESCRIPTION.
+
+    """
+    # unfold lineages
+    lineages = [unfold_lineage(taxid, nodes_tab, *ranks) for taxid in taxid_tab.unique()]
+    lineages = pd.DataFrame(lineages, index = taxid_tab.unique())[list(ranks)]
+    
+    # some records have an assigned taxonomy at a lower rank than the ones specified in ranks
+    # we need to retrieve the real taxid for each record (the lowest taxa within the specified ranks)
+    real_taxids = pd.Series(index=lineages.index) # this will contain the value that needs to replace each "false" taxid
+    for col in lineages.columns:
+        valid_idxs = ~lineages[col].isna().values
+        real_taxids.loc[valid_idxs] = lineages.loc[valid_idxs, col].values
+    
+    # repeated records produced by records with lower rank taxonomic assignments, clear them
+    lineages.index = real_taxids.values.astype(int)
+    lineages.drop_duplicates(inplace=True)
+    # add lineages of higher rank taxa
+    for idx, rk in enumerate(ranks[:-1]):
+        trail = list(ranks)[:idx+1]
+        for tax, subtab in lineages.groupby(rk):
+            lineages.loc[tax, trail] = subtab[trail].iloc[0]
+    lineages.sort_values(list(ranks)[::-1], inplace=True)
+    lineages = lineages.fillna(0).astype(int)
+    return lineages, real_taxids.astype(int)
+
 ranks = 'phylum class order family genus species'.split()
+lineages, real_taxids = build_lineage_table(acc_taxids, nodes_tab, *ranks)
 
-lineages = [unfold_lineage(taxid, nodes_tab, ranks) for taxid in taxids.unique()]
-lineages = pd.DataFrame(lineages, index = taxids.unique())[ranks]
-
-real_taxids = pd.Series(index=lineages.index)
-for col in lineages.columns:
-    valid_idxs = ~lineages[col].isna().values
-    real_taxids.loc[valid_idxs] = lineages.loc[valid_idxs, col].values
-
-lineages.index = real_taxids.values.astype(int)
-lineages.drop_duplicates(inplace=True)
-lineages.sort_values(ranks[::-1], inplace=True)
-lineages = lineages.fillna(0).astype(int)
-for idx, rk in enumerate(ranks[:-1]):
-    trail = ranks[:idx+1]
-    for tax, subtab in lineages.groupby(rk):
-        lineages.loc[tax, trail] = subtab[trail].iloc[0]
 #%% build taxonomy table
-taxonomy = real_taxids.loc[taxids].astype(int)
-taxonomy.index = tax_tab.index
+taxonomy = real_taxids.loc[acc_taxids].astype(int)
+taxonomy.index = source_tax.index
 #%% build name table
 names = names_tab.loc[np.unique(lineages), 'TaxId']
